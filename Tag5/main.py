3a55750ec76d8c42aca141288f141b08244dfbb9
thartmann <thomas.hartmann@jambit.com>
2021-12-05 15:02:49 +0100

    Tag 5 Part 2

--- Tag5/main.py
+++ Tag5/main.py
@@ -7,16 +7,27 @@ def firstTask():
         coordianate1, coordinate2 =  line.split(' -> ')
         x1, y1 = [int(x) for x in coordianate1.split(',')]
         x2, y2 = [int(x) for x in coordinate2.split(',')]
-        xCoordinates = [x1, x2]
-        xCoordinates.sort()
-        x1, x2 = xCoordinates
-        yCoordinates = [y1, y2]
-        yCoordinates.sort()
-        y1, y2 = yCoordinates
         if x1 == x2 or y1 == y2: # only horizontal lines
+            xCoordinates = [x1, x2]
+            xCoordinates.sort()
+            x1, x2 = xCoordinates
+            yCoordinates = [y1, y2]
+            yCoordinates.sort()
+            y1, y2 = yCoordinates
             for x in range(x1,x2+1):
                 for y in range(y1, y2+1):
                    oceanFloor[x][y] = oceanFloor[x][y] +1
+        else:
+            yDirection = 1 if y2> y1 else -1
+            xDirection = 1 if x2> x1 else -1
+            while True:
+                  oceanFloor[x1][y1] = oceanFloor[x1][y1] +1
+                  y1 += yDirection
+                  x1 += xDirection
+                  if (xDirection == 1 and x1 > x2) or (xDirection == -1 and x1<x2):
+                      break
+                  
+
 
     print(oceanFloor)
     print((oceanFloor > 1).sum())
