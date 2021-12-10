7f737e26fbd0fa025a6fee51036b862b6b200324
thartmann <thomas.hartmann@jambit.com>
2021-12-10 13:48:26 +0100

    Tag 10 Part 2

--- Tag10/main.py
+++ Tag10/main.py
@@ -4,8 +4,11 @@ from collections import deque
 def firstTask():
     syntaxScore = 0
     syntaxStack = deque()
+    autoCompleteScores = []
     with open("input.txt") as file:
        while(line := file.readline().strip()):
+           checkAutoComplete(syntaxStack, autoCompleteScores)
+
            syntaxStack = deque()
            for c in line:
                 if c == '{':
@@ -14,6 +17,7 @@ def firstTask():
                 if c == '}':
                     if syntaxStack.pop() != '{':
                         syntaxScore += 1197
+                        syntaxStack.clear()
                         break 
                 if c == '(':
                    syntaxStack.append('(')
@@ -21,6 +25,7 @@ def firstTask():
                 if c == ')':
                     if syntaxStack.pop() != '(':
                         syntaxScore += 3
+                        syntaxStack.clear()
                         break         
                 if c == '<':
                    syntaxStack.append('<')
@@ -28,6 +33,7 @@ def firstTask():
                 if c == '>':
                     if syntaxStack.pop() != '<':
                         syntaxScore += 25137
+                        syntaxStack.clear()
                         break
                 if c == '[':
                    syntaxStack.append('[')
@@ -35,9 +41,34 @@ def firstTask():
                 if c == ']':
                     if syntaxStack.pop() != '[':
                         syntaxScore += 57
+                        syntaxStack.clear()
                         break 
-
+    checkAutoComplete(syntaxStack, autoCompleteScores)
     print(syntaxScore)
+    autoCompleteScores.sort()
+    print(autoCompleteScores)
+    print(autoCompleteScores[int(len(autoCompleteScores)/2)])
+
+def checkAutoComplete(syntaxStack, autoCompleteScores):
+    # check last incomplete Line
+    if syntaxStack:
+        score = 0
+        syntaxStack.reverse()
+        for d in syntaxStack:
+            score *= 5
+            if d == '{':
+                score += 3
+                continue
+            if d == '(':
+                score += 1
+                continue
+            if d == '<':
+                score += 4
+                continue
+            if d == '[':
+                score += 2
+                continue
+        autoCompleteScores.append(score)
                 
                 
 
